e0115ab4b3df3dd7d6c67b01b3c2aebc6c60dc7e
Anna Moskalchuk <anna@crystax.net>
2018-05-03 12:52:05 +0300

    feat: add class Player from class Enemy

--- js/app.js
+++ js/app.js
@@ -1,25 +1,34 @@
 // Enemies our player must avoid
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+  // Variables applied to each of our instances go here,
+  // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+  // The image/sprite for our enemies, this uses
+  // a helper we've provided to easily load images
+  this.sprite = 'images/enemy-bug.png';
+  this.x = -20;
+  this.y = 65;
+
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    this.x = this.x + 2;
+
+  // You should multiply any movement by the dt parameter
+  // which will ensure the game runs at the same speed for
+  // all computers.
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
+
+class Player extends Enemy {};
+
+
 
 // Now write your own player class
 // This class requires an update(), render() and
@@ -35,19 +44,38 @@ Enemy.prototype.render = function() {
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
 
 let allEnemies = [];
-const enemy = new Enemy;
 
-allEnemies.push(enemy);
+const player = new Player;
+player.sprite = 'images/char-boy.png';
+player.x = 3;
+player.y = 50;
 
-const player = new Enemy;
+//player.update;
+
+function createEnemies() {
+  for (let i = 0; i < 2; i++) {
+    const enemy = new Enemy;
+    enemy.y = 50 * i + 150;
+    switch(i) {
+      case 0 : enemy.x = -50;
+        break;
+      case 1 : enemy.x = 20;
+        break;
+    }
+    allEnemies.push(enemy);
+  }
+};
+
+createEnemies();
+
